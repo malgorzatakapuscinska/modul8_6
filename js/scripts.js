1d5e3225f30ed7d314b769343b4a8e2fcfe7638f
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-07 23:50:46 +0100

    Task 7_6 finished

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var a=4,
+	b=6,
+	value;
+
+value = (a * a) - (2 * a * b) + (b * b);
+
+console.log(value);
+
+if (value >0) {
+	console.log('wynik dodatni');
+} else if (value <0) {
+	colnsole.log('Wynik ujemny');
+}
+else {
+	console.log('Zmienna równa zero');
+}
+
+/*  A w tv leci uciekinier z 1987r - początkowe napisy bezbłędne - daleka przyszłość 2019 r :-) */
